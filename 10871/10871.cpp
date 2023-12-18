cc89d8ae4bcf08cbdce14f4cf1e21849c002af0c
Jimin0430 <tangbole0430@gmail.com>
2023-12-18 21:59:38 +0900

    10871 최지민

---
+++ 10871/10871.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, X;
+    cin >> N >>X;
+    vector<int> arr(N);
+    vector<int> result;
+
+    for(int i =0; i<N; i++){
+        cin >> arr[i];
+        if (arr[i] < X){
+            result.push_back(arr[i]);
+        };
+    };
+    for(int i=0;i<result.size();i++){
+        cout << result[i]<<' ';
+    }
+    return 0;
+}
